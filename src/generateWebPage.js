da4d049f28116605d5d8aa15a520e356efed47b5
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-10-16 22:25:58 -0400

    edited logic in generateWebPage to use classes and functions created for constructors. also edited link for style.css as it was not working, now works

--- src/generateWebPage.js
+++ src/generateWebPage.js
@@ -77,7 +77,7 @@ function createHTML(employeeSections) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./dist/style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
@@ -108,20 +108,28 @@ const generateWebPage = function(data) {
     // iterate through and create each applicable employee
     for (let i = 0; i < data.length; i++) {
         let employee = data[i];
+
+        // obtain all info generic to all employees
+        let name = employee.getName();
+        let id = employee.getId();
+        let email = employee.getEmail();
         let employeeType = employee.getRole();
 
         if (employeeType === 'Manager') {
-            let { name, id, email, officeNum } = employee;
+            let officeNum = employee.getOfficeNum();
             const managerSection = createManager(name, id, email, officeNum);
             teamMembersFull.push(managerSection);
+
         } else if (employeeType === 'Engineer') {
-            let { name, id, email, ghUsername } = employee;
+            let ghUsername = employee.getGithub();
             const engineerSection = createEngineer(name, id, email, ghUsername);
             teamMembersFull.push(engineerSection);
+
         } else if (employeeType === 'Intern') {
-            let { name, id, email, school } = employee;
+            let school = employee.getSchool();
             const internSection = createIntern(name, id, email, school);
             teamMembersFull.push(internSection);
+
         } else {
             console.log("How did you get here? (generateWebPage Function)");
         }
